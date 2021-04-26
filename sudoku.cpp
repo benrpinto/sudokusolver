614afab388624baba081041cded23a93e5a71c14
Benji <benrpinto@hotmail.com>
2021-04-26 21:38:43 +1000

    Added file reading capability Added sudoku puzzle class Added base constructor for sudoku puzzle class Added test puzzle

--- sudoku.cpp
+++ sudoku.cpp
@@ -6,8 +6,35 @@
 using namespace std;
 
 int main(int argc, char** argv){
+   SudokuPuzzle myPuzzle;
+
    string filename;
-   cin>>filename;
-   cout<<filename<<"\n";
+   FILE* puzzleFile;
+   getline(cin,filename);
+
+   //extract the puzzle from the file
+   puzzleFile = fopen(filename.c_str(),"r");
+   if(!puzzleFile){
+      cout<<"file not found\n";
+   }else{
+      char ch;
+      int digit;
+      for(int row = 0; row < 9; row++){
+         for(int col = 0; col < 9; col++){
+            ch = getc(puzzleFile);
+            if(ch >= '1' && ch <= '9'){
+               digit = ch - '0';
+            }else{
+               digit = 0;
+            }
+            cout<<digit;
+         }
+         //read and throw out the newline
+         getc(puzzleFile);
+         cout<<"\n";
+      }
+   }
+
+   fclose(puzzleFile);
    return 0;
 }
--- sudoku.h
+++ sudoku.h
@@ -1,14 +1,39 @@
-class GroupOfNine;
 
-class DigitBox{
-      short Filled = 0;
+class SudokuPuzzle{
+   public:
+   class GroupOfNine;
+   class DigitBox{
+      public:
+      short filled;
       bool options[9];
       GroupOfNine *row;
       GroupOfNine *col;
       GroupOfNine *box;
-};
+   };
 
-class GroupOfNine{
-   bool contains[9];
-   DigitBox contents;
+   class GroupOfNine{
+      public:
+      bool contains[9];
+      DigitBox *contents[9];
+   };
+
+   DigitBox individual[9][9];
+   GroupOfNine rows[9];
+   GroupOfNine cols[9];
+   GroupOfNine boxes[3][3];
+
+      SudokuPuzzle(){
+         for (int irow = 0; irow < 9; irow++){
+            for(int icol = 0; icol < 9; icol++){
+               individual[irow][icol].filled = 0;
+               individual[irow][icol].row = &rows[irow];
+               individual[irow][icol].col = &cols[icol];
+               individual[irow][icol].col = &boxes[irow/3][icol/3];
+               rows[irow].contents[icol] = &individual[irow][icol];
+               cols[icol].contents[irow] = &individual[irow][icol];
+               boxes[irow/3][icol/3].contents[3*irow%3 + icol%3] = &individual[irow][icol];
+            }
+         }
+      }
+
 };
