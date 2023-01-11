5ffc54f2879effc97c405a9c4d6fb81acef50015
benrpinto <benrpinto@gmail.com>
2023-01-11 16:44:42 +1100

    Started on python version of sudoku solver Initialises basic sudoku puzzle and prints it

---
+++ pySolver/main.py
@@ -0,0 +1,34 @@
+from constant import *
+from sudoku import *
+from DFSSolve import *
+from test import *
+
+#intentionally including types, even though python doesn't require it
+
+def main():
+    print("enter puzzle filename:")
+    filename:str = input()
+    inputDigits:list[int] = []
+    try:
+        with open(filename,"r") as puzzleFile:
+            puzzleFile:io.TextIOWrapper
+            ch:str = "a"
+            while (ch != ""):
+                ch = puzzleFile.read(1)
+                if((ch >= '1') and (ch <= '9')):
+                    inputDigits.append(int(ch))
+                elif(ch == " "):
+                    inputDigits.append(0)
+
+            #print(len(inputDigits))
+            #print(inputDigits)
+
+    except IOError:
+      print("Could not read file:", filename)
+
+    myPuzzle:SudokuPuzzle = SudokuPuzzle(inputDigits)
+    print(myPuzzle.display())
+
+
+if __name__ == "__main__":
+    main()
