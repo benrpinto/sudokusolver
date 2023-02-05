ffdcf8ecb612caa6d69574b121cea437e64535a1
benrpinto <benrpinto@Veritas2>
2023-02-05 23:19:34 +1100

    Added file reading capability to rustSolver Added function to open the file, and convert the contents to a Vec<u8>

--- rustSolver/sudoku_cargo/src/main.rs
+++ rustSolver/sudoku_cargo/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 pub mod sudoku;
 
@@ -12,23 +13,37 @@ fn main() {
 fn test() {
     println!("running tests");
 
-    let my_var : sudoku::Cell = sudoku::Cell::Filled(5);
-    println!("{}",my_var.display());
-    let v:Vec<u8> = vec![
-    1,2,3,4,5,6,7,8,9,
-    7,8,9,1,2,3,4,5,6,
-    4,5,6,7,8,9,1,2,3,
-    2,3,4,5,6,7,8,9,1,
-    8,9,1,2,3,4,5,6,7,
-    5,6,7,8,9,1,2,3,4,
-    3,4,5,6,7,8,9,1,2,
-    9,1,2,3,4,5,6,7,8,
-    6,7,8,9,1,2,3,4,5,
-    ];
+    let question_files:Vec<&str> = vec![
+            "../../test puzzles/easy01.txt",
+            "../../test puzzles/easy02.txt",
+            "../../test puzzles/tough01.txt",
+            "../../test puzzles/AI Escargot.txt",
+            ];
+    let answer_files:Vec<&str> = vec![
+            "../../test puzzles/easy01sol.txt",
+            "../../test puzzles/easy02sol.txt",
+            "../../test puzzles/tough01sol.txt",
+            "../../test puzzles/AI Escargotsol.txt",
+            ];
+
+    let v:Vec<u8> = filepath_to_numbers(question_files[0].to_string());
+
     let my_puzzle:sudoku::SudokuPuzzle = sudoku::SudokuPuzzle::new(v);
     println!("{}",my_puzzle.display());
-
-    
-
-    
 }
+
+
+fn filepath_to_numbers(filepath: String)-> Vec<u8>{
+    let mut to_return:Vec<u8> = vec![];
+    let raw_contents:String = fs::read_to_string(filepath).expect("unable to read error");
+    let byte_contents: &[u8] = raw_contents.as_bytes();
+    for (_,&item) in byte_contents.iter().enumerate(){
+        match item {
+            b' ' => to_return.push(0),
+            b'1'..=b'9' => to_return.push((item - b'0') as u8),
+            _ =>(),
+        }
+    }
+    to_return
+}
+
