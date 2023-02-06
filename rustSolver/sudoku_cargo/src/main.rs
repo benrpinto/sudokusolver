e7915522176d7d4095cdb1592d5d608598a966ee
benrpinto <benrpinto@gmail.com>
2023-02-06 15:31:10 +1100

    Implemented more graceful handling of file reading errors for Rust Solver

--- rustSolver/sudoku_cargo/src/main.rs
+++ rustSolver/sudoku_cargo/src/main.rs
@@ -26,24 +26,37 @@ fn test() {
             "../../test puzzles/AI Escargotsol.txt",
             ];
 
-    let v:Vec<u8> = filepath_to_numbers(question_files[0].to_string());
+    let raw_v:Result<Vec<u8>,std::io::Error> = filepath_to_numbers(question_files[0].to_string());
 
-    let my_puzzle:sudoku::SudokuPuzzle = sudoku::SudokuPuzzle::new(v);
-    println!("{}",my_puzzle.display());
+    if let Ok(v) = raw_v {
+        let my_puzzle:sudoku::SudokuPuzzle = sudoku::SudokuPuzzle::new(v);
+        println!("{}",my_puzzle.display());
+    }else if let Err(_) = raw_v{
+        let mut error_message:String = "Could not read file:".to_string();
+        error_message.push_str(&question_files[0]);
+        println!("{}",&error_message);
+    }
 }
 
 
-fn filepath_to_numbers(filepath: String)-> Vec<u8>{
+fn filepath_to_numbers(filepath: String)-> Result<Vec<u8>,std::io::Error>{
+    let mut wrapped_return = Ok(vec![]);
     let mut to_return:Vec<u8> = vec![];
-    let raw_contents:String = fs::read_to_string(filepath).expect("unable to read error");
-    let byte_contents: &[u8] = raw_contents.as_bytes();
-    for (_,&item) in byte_contents.iter().enumerate(){
-        match item {
-            b' ' => to_return.push(0),
-            b'1'..=b'9' => to_return.push((item - b'0') as u8),
-            _ =>(),
+    let wrapped_contents:Result<String,std::io::Error> = fs::read_to_string(filepath);
+
+    if let Ok(raw_contents) = wrapped_contents{
+        let byte_contents: &[u8] = raw_contents.as_bytes();
+        for (_,&item) in byte_contents.iter().enumerate(){
+            match item {
+                b' ' => to_return.push(0),
+                b'1'..=b'9' => to_return.push((item - b'0') as u8),
+                _ =>(),
+            }
         }
+        wrapped_return = Ok(to_return);
+    }else if let Err(a) = wrapped_contents{
+        wrapped_return = Err(a);
     }
-    to_return
+    wrapped_return
 }
 
